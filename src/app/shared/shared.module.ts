2fc360d9611eeedfa907fde649ab671097536bf0
machariamarigi <machariamarigi@gmail.com>
2019-06-01 16:00:19 +0300

    share the custome material module and Angular layout module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { MaterialModule } from '../material/material.module';
 import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [CommonModule, FlexLayoutModule],
-  exports: [LayoutComponent]
+  imports: [CommonModule, FlexLayoutModule, MaterialModule],
+  exports: [LayoutComponent, MaterialModule, FlexLayoutModule]
 })
 export class SharedModule {}
