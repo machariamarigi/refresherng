f00698fc1f4e8cd9aa2d63ef5c20f795c60dbf4f
machariamarigi <machariamarigi@gmail.com>
2019-06-01 16:07:26 +0300

    add tabs to display content

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [],
-  exports: [],
-  imports: [CommonModule]
+  imports: [CommonModule, MatTabsModule],
+  exports: [MatTabsModule]
 })
 export class MaterialModule {}
