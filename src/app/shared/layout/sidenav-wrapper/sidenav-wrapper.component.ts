51e112c7f7360fce6e67e2c27f557133357a08c3
machariamarigi <machariamarigi@gmail.com>
2019-06-01 17:06:18 +0300

    generate sidenav wrapper and use it

---
+++ src/app/shared/layout/sidenav-wrapper/sidenav-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-wrapper',
+  templateUrl: './sidenav-wrapper.component.html',
+  styleUrls: ['./sidenav-wrapper.component.scss']
+})
+export class SidenavWrapperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,16 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MaterialModule } from '../material/material.module';
 import { ContentWrapperComponent } from './layout/content-wrapper/content-wrapper.component';
+import { SidenavWrapperComponent } from './layout/sidenav-wrapper/sidenav-wrapper.component';
 
 @NgModule({
-  declarations: [ ContentWrapperComponent],
+  declarations: [ContentWrapperComponent, SidenavWrapperComponent],
   imports: [CommonModule, FlexLayoutModule, MaterialModule],
-  exports: [MaterialModule, FlexLayoutModule, ContentWrapperComponent]
+  exports: [
+    MaterialModule,
+    FlexLayoutModule,
+    ContentWrapperComponent,
+    SidenavWrapperComponent
+  ]
 })
 export class SharedModule {}
