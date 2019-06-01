44c961fe9e7a41aad6b238790167a6018deefd00
machariamarigi <machariamarigi@gmail.com>
2019-06-01 14:51:48 +0300

    add core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material/material.module';
-import { LayoutComponent } from './layout/layout.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [AppComponent, LayoutComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MaterialModule,
-    FlexLayoutModule
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
